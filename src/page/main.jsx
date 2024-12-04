df62a426611a84db191aa22f79bfda35540641a7
narim <ehdrkd648@naver.com>
2024-12-04 23:51:16 +0900

    feat: main page setting

---
+++ src/page/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/main.css'
+
+function Main() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-9xl font-bold text-green">portfolio</p>
+    </div>
+  );
+}
+
+export default Main;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  //src폴더 내 모든 react 파일에 적용하겠다.
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#355E3B",
+      },
+    },
   },
   plugins: [],
 }
